76fb4fe3d04cb1109b8d5ee50f9b0c13416d0ace
zouzouzou11 <1096174991@qq.com>
2021-10-09 10:13:02 +0800

    init

---
+++ packages/main/index.ts
@@ -0,0 +1,14 @@
+class App {
+  private _m: number = 1;
+  public m: number = 2;
+  static a: number = 3;
+  constructor() {}
+
+  setM() {}
+
+  static setA() {}
+  
+  private _s() {}
+}
+
+export { App };
