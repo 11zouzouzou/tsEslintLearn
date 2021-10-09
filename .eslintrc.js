18a3c71c90c99003df328ce4dc160a1ec771a7ea
zouzouzou11 <1096174991@qq.com>
2021-10-09 10:40:31 +0800

    test hook

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,7 @@ module.exports = {
     node: true,
   },
   rules: {
-    "@typescript-eslint/no-inferrable-types": "off",
+    // "@typescript-eslint/no-inferrable-types": "off",
     '@typescript-eslint/naming-convention': [
       'warn',
       {
